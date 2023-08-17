cba391c7a0c564d4b288a78da565c7ede80a2e40
Clifford Mwale <cliffmwale97@gmail.com>
2023-08-17 11:01:34 +0200

    Commit

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -35,7 +35,7 @@ module.exports = class HouseholdsController{
 
                 let District = await this.district.Read({District_Code: EPAs.District})
                 District = District[0]
-                let District_Name = District.split('')
+                let District_Name = District.District_Name.split('')
                 District_Name= District_Name[0].toUpperCase()
                 console.log("Constitutency Search: ", {DISTRICT:District.District_Name})
                 let Constituencies = await this.constituency.Read({DISTRICT:District.District_Name.toUpperCase()})
