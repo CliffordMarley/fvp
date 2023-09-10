df7550b4d7e4596748cb4d11d22ff76ec4f8806a
Clifford Mwale <cliffmwale97@gmail.com>
2023-09-10 05:55:31 +0200

    Commit

--- helpers/cache.helper.js
+++ helpers/cache.helper.js
@@ -1,23 +1,67 @@
 const crypto = require('crypto');
-const { createClient } = require('redis');
+const {createClient} = require('redis')
+
 
 const redisConnect = () => {
-  const redisClient = createClient();
+    const redisClient = createClient();
+  
+    redisClient.on('connect', () => {
+      console.log('Connection to Redis Database was successful!');
+    });
+  
+    redisClient.on('error', (err) => {
+      console.error('Error connecting to Redis:', err);
+      // Close the client on error to prevent further use
+      redisClient.quit();
+    });
+  
+    return redisClient;
+  };
+  
 
-  redisClient.on('connect', () => {
-    console.log('Connection to Redis Database was successful!');
-  });
 
-  redisClient.on('error', (err) => {
-    console.error('Error connecting to Redis:', err);
-    // Close the client on error to prevent further use
-    redisClient.quit();
-  });
+const Hash = (jsonString)=>{
+    try {
+        // Create an MD5 hash object
+        const md5Hash = crypto.createHash('md5');
 
-  return redisClient;
-};
+        // Update the hash object with the JSON string
+        md5Hash.update(jsonString);
 
+        // Calculate the MD5 hash and return it as a hexadecimal string
+        const hashResult = md5Hash.digest('hex');
+        
+        return hashResult;
+    } catch (error) {
+        console.error('Error hashing JSON:', error);
+        return null;
+    }
+}
 
+const getCache = async (key)=>{
+    let client = null
+    try{
+        client = await redisConnect()
+
+        if(client){
+            client.get(key, (err, result)=>{
+                if(err){
+                    console.log(err)
+                    return false
+                }else{
+                    return JSON.parse(result)
+                }
+            })
+        }else{
+            return false
+        }
+    }catch(err){
+        console.log(err.message)
+        return false
+    }finally{
+        //client != null ? client.disconnect() : {}
+    }
+}
 
 const setCache = async (key, value) => {
     let client = null;
